2c0af082a0964545a0037f6916877026f53dc84f
tabrizgulmammadov <tebrizg57@gmail.com>
2025-09-20 09:53:18 +0400

    Init project, create scaffolding APIs
    Formatted responses and error cases
    
    Added validation, encoding
    
    Completed CRUD operations on movies
    
    Added pagination, filters, and sort on getting all movies endpoint
    
    Implemented rate limiting
    
    Implemented gracefull shutdown
    
    Implemented user registration
    
    Implemented sending email
    
    Implemented activation of user logic
    
    Implemented authentication of user logic
    
    Implemented permission-based authorization

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# greenlight
+    # mac os/linux
+    # export GREENLIGHT_DB_DSN='postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable'
+    # migrate create -seq -ext=.sql -dir=./migrations create_movies_table
+    # migrate create -seq -ext=.sql -dir=./migrations add_movies_check_constraints
+    # migrate -path=./migrations -database=$GREENLIGHT_DB_DSN up
+    # migrate -path=./migrations -database=$GREENLIGHT_DB_DSN version
+    # migrate -path=./migrations -database=$GREENLIGHT_DB_DSN goto 1
+    # migrate -path=./migrations -database=$GREENLIGHT_DB_DSN down 1
+    # migrate -path=./migrations -database=$EXAMPLE_DSN down -- down all migratins
+
+    # windows
+    # $env:GREENLIGHT_DB_DSN = "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable"
+    # migrate create -seq -ext .sql -dir ./migrations create_movies_table
+    # migrate create -seq -ext .sql -dir ./migrations add_movies_check_constraints
+    # migrate -path ./migrations -database $env:GREENLIGHT_DB_DSN up
+    # migrate -path ./migrations -database $env:GREENLIGHT_DB_DSN version
+    # migrate -path ./migrations -database $env:GREENLIGHT_DB_DSN goto 1
+    # migrate -path ./migrations -database $env:GREENLIGHT_DB_DSN down 1
+    # migrate -path ./migrations -database $env:GREENLIGHT_DB_DSN down
---
+++ cmd/api/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"log/slog"
+	"os"
+	"sync"
+	"time"
+
+	_ "github.com/lib/pq"
+	"greenlight.tabriz.gulmammadov.net/internal/data"
+	"greenlight.tabriz.gulmammadov.net/internal/mailer"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+	models data.Models
+	mailer *mailer.Mailer
+	wg     sync.WaitGroup
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiters")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "baf3c30dbc8dd5", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "e26985c8241c05", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@tabrizgulmammadov.com>", "SMTP sender")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+
+	mailer, err := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+		models: data.MewModels(db),
+		mailer: mailer,
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
