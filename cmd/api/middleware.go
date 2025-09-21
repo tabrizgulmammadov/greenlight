258f70053ee6a21137c9f03eabedfb1079ab395f
tabrizgulmammadov <tebrizg57@gmail.com>
2025-09-21 10:20:43 +0400

    Formatted responses and error cases

---
+++ cmd/api/middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			err := recover()
+			if err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrorResponse(w, r, fmt.Errorf("%v", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
---
+++ internal/data/movies.go
@@ -0,0 +1,15 @@
+package data
+
+import (
+	"time"
+)
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title,omitzero"`
+	Year      int32     `json:"year,omitzero"`
+	Runtime   Runtime   `json:"runtime,omitzero"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
